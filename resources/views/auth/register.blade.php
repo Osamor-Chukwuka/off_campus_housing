14c6edf8c417aa2bfdbb329bd2e25edddf1c81f3
Osamor Chukwuka <osamorchukwuka15@gmail.com>
2023-03-25 23:53:30 +0100

    added an Account type feature

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,18 @@
                             </div>
                         </div>
 
+
+                        <div class="row mb-3">
+                            <label for="Account-type" class="col-md-4 col-form-label text-md-end">Land Lord or Student</label>
+
+                            <div class="col-md-6">
+                                <select name="Account-type" id="">
+                                    <option value="LandLord">Land Lord</option>
+                                    <option value="Student">Student</option>
+                                </select>
+                            </div>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
