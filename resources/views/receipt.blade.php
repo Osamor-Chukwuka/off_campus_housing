676b8acfacd147ccb12958896108ddf55f008f98
Osamor Chukwuka <osamorchukwuka15@gmail.com>
2023-04-02 00:15:41 +0100

    created the receipt with dummy data

--- resources/views/receipt.blade.php
+++ resources/views/receipt.blade.php
@@ -9,17 +9,52 @@
     <title>Receipt</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container ">
         <h1 class="text-center">Your Receipt</h1>
 
-        <div class="row">
-            <div class="col-auto fs-3">
-                <ul>
+        <div class="row border border-4" style="width: 50%;">
+            <div class="col fs-3 align-self-center">
+                <ul class="receipt_list">
                     <li>House type:</li>
                     <li>House address:</li>
+                    <li>Price:</li>
+                    <li>Duration</li>
+                    <li>Land Lord:</li>
+                    <li>reference number:</li>
+                </ul>
+            </div>
+            
+            <div class="col">
+                <div class="d-flex" style="height: 400px;">
+                    <div class="vr"></div>
+                </div>  
+
+                <button class="btn btn-outline-success">Print</button>
+            </div>
+
+            <div class="col fs-3 align-self-center">
+                <ul class="receipt_list">
+                    <li>bungalow</li>
+                    <li>bungalow</li>
+                    <li>bungalow</li>
+                    <li>bungalow</li>
+                    <li>bungalow</li>
+                    <li>bungalow</li>
+                    <li>bungalow</li>
+                    <li>bungalow</li>
+                    
                 </ul>
             </div>
         </div>
+        
     </div>
 </body>
 </html>
+
+
+
+<style>
+    .receipt_list{
+        list-style-type:none;
+    }
+</style>
