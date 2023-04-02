a928a8ffab460326a982b110ba30b35eae5ba046
Osamor Chukwuka <osamorchukwuka15@gmail.com>
2023-04-02 22:42:40 +0100

    modified the receipt page

--- resources/views/receipt.blade.php
+++ resources/views/receipt.blade.php
@@ -2,59 +2,65 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Receipt</title>
 </head>
+
 <body>
     <div class="container ">
         <h1 class="text-center">Your Receipt</h1>
 
-        <div class="row border border-4" style="width: 50%;">
-            <div class="col fs-3 align-self-center">
-                <ul class="receipt_list">
-                    <li>House type:</li>
-                    <li>House address:</li>
-                    <li>Price:</li>
-                    <li>Duration</li>
-                    <li>Land Lord:</li>
-                    <li>reference number:</li>
-                </ul>
-            </div>
-            
-            <div class="col">
-                <div class="d-flex" style="height: 400px;">
-                    <div class="vr"></div>
-                </div>  
+        <table class="table table-responsive     table-striped ms-5 container ps-5 border border-3" style="width: 40%;">
+            {{-- <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead> --}}
+            <tbody>
+                <tr>
+                    <th scope="row"></th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">House address:</th>
+                    <td>{{$house[0]->address}}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Price:</th>
+                    <td>N{{$house[0]->price}}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Duration:</th>
+                    <td colspan="2">per / {{$house[0]->duration}}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Land Lord:</th>
+                    <td colspan="2">{{$landLord[0]->name}}</td>
+                </tr>
+                <tr>
+                    <th scope="row">reference number:</th>
+                    <td colspan="2">{{$reference}}</td>
+                </tr>
 
-                <button onclick="window.print()" class="btn btn-outline-success">Print</button>
-            </div>
+                <tr>
+                    <th> </th>
+                    <td><button onclick="window.print()" class="btn btn-outline-success float-end">Print</button></td>
+                </tr>
+                
+            </tbody>
+        </table>
 
-            <div class="col fs-3 align-self-center">
-                <ul class="receipt_list">
-                    <li>bungalow</li>
-                    <li>bungalow</li>
-                    <li>bungalow</li>
-                    <li>bungalow</li>
-                    <li>bungalow</li>
-                    <li>bungalow</li>
-                    <li>bungalow</li>
-                    <li>bungalow</li>
-                    
-                </ul>
-            </div>
-        </div>
         
+
     </div>
 </body>
+
 </html>
 
-
-
-<style>
-    .receipt_list{
-        list-style-type:none;
-    }
-</style>
